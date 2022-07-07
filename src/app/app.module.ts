376131acb31f18ac7259de8bddf3cdb2cf8af718
Dmitriy Permyakov <dmitrypermyakov1990@gmail.com>
2022-07-07 16:50:38 +0300

    Implemented main layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TeamsComponent } from './teams/teams.component';
+import { LeaguesComponent } from './leagues/leagues.component';
+import { TeamCardComponent } from './team-card/team-card.component';
+import { LeagueCardComponent } from './league-card/league-card.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TeamsComponent,
+    LeaguesComponent,
+    TeamCardComponent,
+    LeagueCardComponent,
+    SearchComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
