426d19b46f621ae6b58901f527780899160169f1
Dmitriy Permyakov <dmitrypermyakov1990@gmail.com>
2022-09-13 20:00:24 +0300

    Implemented registration

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { RegisterRequest } from '../interfaces/interfaces';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
+  public form: FormGroup;
 
-  constructor() { }
+  private authSubscription: Subscription;
+
+  constructor(private router: Router, private auth: AuthService) {
+    this.form = new FormGroup({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      passwordConfirm: new FormControl('', Validators.required)
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  toMainPage() {
+    this.router.navigate(['/leagues'])
+  }
+  clear() {
+    this.form.reset();
+  }
+
+  submit() {
+    const regRequest: RegisterRequest = {
+      email: this.form.value.email,
+      password: this.form.value.password,
+      passwordConfirm: this.form.value.passwordConfirm
+    }
+
+    this.authSubscription = this.auth.register(regRequest)
+      .subscribe(() => {
+        this.form.reset();
+        this.router.navigate(['/leagues']);
+      },
+      (error) => console.log(error));
+  }
+
+  ngOnDestroy(): void {
+    if(this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
   }
 
 }
